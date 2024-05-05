25ead58570d9d9a03683fc4dbdccc662f485517e
Hafizh Pratama <hafizhpratama99@gmail.com>
2024-05-05 14:16:28 +0800

    fix: build update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import { AuthProvider } from './contexts/AuthContext'
